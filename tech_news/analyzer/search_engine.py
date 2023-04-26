486b35ae4886291ccc80a736852bfe87f4355fc1
Ruham Leal <ruhamxlpro@hotmail.com>
2023-04-26 12:03:21 -0300

    feat: Implements search_by_date and search_by_category functions to get filtered news

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,6 @@
 from ..database import search_news
+from datetime import datetime
+import re
 
 
 def search_by_title(title: str):
@@ -11,11 +13,38 @@ def search_by_title(title: str):
     return news_list
 
 
-# Requisito 8
+def format_date(date):
+    rgx = r"\d{4}-\d{2}-\d{2}"
+
+    try:
+        if re.match(rgx, date):
+            return datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        else:
+            raise Exception
+
+    except (Exception):
+        raise ValueError("Data com formato inválido")
+
+
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    news_list = list()
+    formated_date = format_date(date)
+
+    query = {"timestamp": formated_date}
+
+    for news in search_news(query):
+        news_list.append((news["title"], news["url"]))
+
+    return news_list
 
 
-# Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    rgx = re.compile(category, re.IGNORECASE)
+
+    news_list = list()
+    query = {"category": {"$regex": rgx}}
+
+    for news in search_news(query):
+        news_list.append((news["title"], news["url"]))
+
+    return news_list
