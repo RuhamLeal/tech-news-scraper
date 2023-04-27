16228e0c759a853006c92e346e44533e88add21f
Ruham Leal <ruhamxlpro@hotmail.com>
2023-04-27 00:31:45 -0300

    feat: Implements top_5_categories function to get first five better evaluated categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,24 @@
-# Requisito 10
+from .search_engine import search_by_category
+from ..database import find_news
+
+
+def find_all_categories():
+    categories = set()
+
+    for new in find_news():
+        categories.add(new["category"])
+
+    return categories
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    categories = find_all_categories()
+    evaluations = list()
+
+    for category in categories:
+        category_qty = tuple((category, len(search_by_category(category))))
+        evaluations.append(category_qty)
+
+    evaluations.sort(key=lambda a: (-a[1], a[0]))
+
+    return [evaluation[0] for evaluation in evaluations[:5]]
